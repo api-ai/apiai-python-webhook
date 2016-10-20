f07e9d3f95d7e5721c12923378b97840ae265027
tedgoddard <ted.goddard@robotsandpencils.com>
2016-10-20 11:28:56 -0600

    grammar formatting

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import urllib
 import json
 import os
 from string import Formatter
+from collections import namedtuple
 from neo4j.v1 import GraphDatabase, basic_auth
 from neo4j.v1.types import Node, Relationship
 from pattern.en import conjugate
@@ -25,6 +26,45 @@ print(graphenedb_user)
 print(graphenedb_pass)
 
 driver = GraphDatabase.driver(graphenedb_url, auth=basic_auth(graphenedb_user, graphenedb_pass))
+
+
+def objectify(dict):
+    fields = ' '.join(dict.keys())
+    DictTuple = namedtuple('DictTuple', fields)
+    return DictTuple(**dict)
+
+class GrammaticalString(unicode):
+    def __format__(self, spec):
+        #more work is needed if we want to combine grammar conversion with other formatting
+        value = unicode(self)
+        grammarDirective = spec
+        return conjugate(value, grammarDirective)
+
+
+class GrammarFormatter(Formatter):
+    def convert_field(self, value, conversion):
+        if 'g' == conversion:
+            grammarDirective = conversion[1:]
+            return GrammaticalString(value)
+        else:
+            return Formatter.convert_field(self, value, conversion)
+
+    def get_value(self, key, args, kwds):
+        if isinstance(key, str) or isinstance(key, unicode):
+            try:
+                return kwds[key]
+            except KeyError:
+                #allow arbitrarily named format specifications according to Pattern aliases
+                return key
+        else:
+            print("not string key ", key)
+            return Formatter.get_value(self, key, args, kwds)
+
+
+grammify = GrammarFormatter().format
+print(grammify('She {verb!g:{3sg}}, I {verb!g:{inf}}, it is {verb!g:{1sgp}}.', verb='sells'))
+print(grammify(u'Indeed, {p.vendor} does {p.offering!g:{inf}} {p.product}', p = objectify({'product': 'iPhone', 'vendor': 'SHI', 'offering': 'SELLS'})));
+
 
 @app.route('/')
 def root():
@@ -152,8 +192,8 @@ def findQuery(req):
     resultList = grapheneQuery(concreteQuery)
 
     speech = ""
-    if len(resultList) == 0 :
-        speech += failedMessage % tuple(queryArgList)
+    if len(resultList) == 0:
+        speech += grammify(failedMessage, p = objectify(parameters))
 
     for record in resultList:
         print(record)
@@ -189,33 +229,6 @@ def grapheneQuery(query):
     session.close()
     return list(resultDB)
 
-class GrammaticalString(unicode):
-    def __format__(self, spec):
-        #more work is needed if we want to combine grammar conversion with other formatting
-        value = unicode(self)
-        grammarDirective = spec
-        return conjugate(value, grammarDirective)
-
-class GrammarFormatter(Formatter):
-    def convert_field(self, value, conversion):
-        if 'g' == conversion:
-            grammarDirective = conversion[1:]
-            return GrammaticalString(value)
-        else:
-            return Formatter.convert_field(self, value, conversion)
-
-    def get_value(self, key, args, kwds):
-        if isinstance(key, str):
-            try:
-                return kwds[key]
-            except KeyError:
-                #allow arbitrarily named format specifications according to Pattern aliases
-                return key
-        else:
-            return Formatter.get_value(self, key, args, kwds)
-
-fmt = GrammarFormatter().format
-print(fmt('She {verb!g:{3sg}}, I {verb!g:{inf}}, it is {verb!g:{1sgp}}.', verb='sells'))
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
