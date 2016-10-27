4c6e17db6651e2a7bb779331be4c9fd3eb420d5d
chrisvenables <chrisvenables@users.noreply.github.com>
2016-10-28 07:33:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,22 +30,12 @@ def webhook():
 
 def processRequest(req):
     if req.get("result").get("action") != "current_price":
-        speech = "couldn't get the action right"
-        return {
-            "speech": speech,
-            "displayText": speech,
-            "source": "apiai-webhook-test-asxdata-cv"
-        }
-
-    url = makeURL(req)
+        return {}
+    url = "http://data.asx.com.au/data/1/share/BHP/"
+    #url = makeURL(req)
     if url is None:
-        speech = "couldn't get the url right"
-        return {
-            "speech": speech,
-            "displayText": speech,
-            "source": "apiai-webhook-test-asxdata-cv"
-        }
-    #yql_url = "http://data.asx.com.au/data/1/share/BHP/"
+        return {}
+    
     result = urllib.urlopen(url).read()
     data = json.loads(result)
     res = makeWebhookResult(data)
@@ -63,59 +53,19 @@ def makeURL(req):
     fullURL = baseurl + urllib.urlencode({'q': stock}) + "/"
     return fullURL
 
-#def makeWebhookResult(data):
-#    query = data.get('query')
-#    if query is None:
-#        return {}
-#
-#    result = query.get('results')
-#    if result is None:
-#        return {}
-#
-#    channel = result.get('channel')
-#    if channel is None:
-#        return {}
-#
-#    item = channel.get('item')
-#    location = channel.get('location')
-#    units = channel.get('units')
-#    if (location is None) or (item is None) or (units is None):
-#        return {}
-#
-#    condition = item.get('condition')
-#    if condition is None:
-#        return {}
-#
-#    # print(json.dumps(item, indent=4))
-#
-#    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
-#             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
-#
-#    print("Response:")
-#    print(speech)
-#
-#    return {
-#        "speech": speech,
-#        "displayText": speech,
-#        # "data": data,
-#        # "contextOut": [],
-#        "source": "apiai-weather-webhook-sample"
-#    }
-
-
 
 def makeWebhookResult(data):
     code = data.get('code')
-    #if code is None:
-    #    return {}
+    if code is None:
+        return {}
 
     last_price = data.get('last_price')
-    #if last_price is None:
-    #    return {}
+    if last_price is None:
+        return {}
 
     change_in_percent = data.get('change_in_percent')
-    #if change_in_percent is None:
-    #    return {}
+    if change_in_percent is None:
+        return {}
 
     speech = "The current price for " + code + " is " + str(last_price) + " (percentage change " + str(change_in_percent) + ")"
 
