686f25edab4e2f0bac4f606d8e47064adf6ecb3f
chrisvenables <chrisvenables@users.noreply.github.com>
2016-10-28 20:25:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,74 +15,52 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = processRequest(req)
-
+    res = processRequest(req)   #Receive data from api.ai and retrieve request data
     res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
+    r = make_response(res)      #Use retrieved data to form response to api.ai
     r.headers['Content-Type'] = 'application/json'
     return r
 
 
-def processRequest(req):
+def processRequest(req):    #Parse data provided to script and retrieve request data
     if req.get("result").get("action") != "current_price":
         return {}
     #url = "http://data.asx.com.au/data/1/share/BHP/"
-    url = makeURL(req)
+    url = makeURL(req)  #construct request url
     if url is None:
         return {}
-    
-    result = urllib.urlopen(url).read()
-    data = json.loads(result)
-    res = makeWebhookResult(data)
+    result = urllib.urlopen(url).read() #read target url
+    data = json.loads(result)   #grab target url json data
+    res = makeWebhookResult(data)   #process data to pass back to api.ai
     return res
 
 
-def makeURL(req):
-    baseurl = "http://data.asx.com.au/data/1/share/"
-    result = req.get("result")
+def makeURL(req):   #construct request url
+    baseurl = "http://data.asx.com.au/data/1/share/"    #url root
+    result = req.get("result")      #grab data from api.ai input
     parameters = result.get("parameters")
-    stock = parameters.get("stock")
+    stock = parameters.get("ASX_stock")
     if stock is None:
         return None
-    fullURL = baseurl + stock + "/"
+    fullURL = baseurl + stock + "/"     #build url
     return fullURL
 
 
-def makeWebhookResult(data):
-    code = data.get('code')
+def makeWebhookResult(data):    #process data to pass back to api.ai
+    code = data.get('code')     #get data
     if code is None:
         return {}
-
     last_price = data.get('last_price')
     if last_price is None:
         return {}
-
     change_in_percent = data.get('change_in_percent')
     if change_in_percent is None:
         return {}
-
+    #build output string
     speech = "The current price for " + code + " is " + str(last_price) + " (percentage change " + str(change_in_percent) + ")"
-
-    print("Response:")
-    print(speech)
-
-    return {
-        "speech": speech,
-        "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
-        "source": "apiai-webhook-test-asxdata-cv"
-    }
+    return {"speech": speech, "displayText": speech, "source": "cv-asxdata"}
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
     app.run(debug=False, port=port, host='0.0.0.0')
